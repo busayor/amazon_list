35f0c0244b6007fb8133cd5d48e1315512c1fbce
Oluwabusayo Folaranmi <oluwabusayofolaranmi@Olayinkas-MacBook-Pro.local>
2020-12-02 21:27:27 +0100

    fixed index error problem

--- my_app/views.py
+++ my_app/views.py
@@ -1,4 +1,5 @@
 import requests
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 from urllib.parse import quote_plus
@@ -25,21 +26,31 @@ def new_search(request):
     # print(error_listing)
     soup = BeautifulSoup(data, features='html.parser')
     post_listings = soup.find_all('div', {'class': 'col'})
-    post_title = post_listings[0].find(class_='ebayui-ellipsis-2').text
-    post_url = post_listings[0].find('a').get('href')
-    post_price = post_listings[0].find(class_='first').text
-    post_img = post_listings[0].find('img').get('src')
+    # post_title = post_listings[0].find(class_='ebayui-ellipsis-2').text
+    # post_url = post_listings[0].find('a').get('href')
+    # post_price = post_listings[0].find(class_='first').text
+    # post_img = post_listings[0].find('img').get('src')
 
     final_postings = []
 
-    for post in post_listings:
-        post_title = post.find(class_='ebayui-ellipsis-2').text
-        post_url = post.find('a').get('href')
-        # what is the underscore after class for?????
-        post_price = post.find(class_='first').text
-        post_img = post.find('img').get('src')
+    if post_listings:
+        for post in post_listings:
+            post_title = post.find(class_='ebayui-ellipsis-2').text
+            post_url = post.find('a').get('href')
+            if post.find(class_='first'):
+                post_price = post.find(class_='first').text
+            else:
+                post_price = 'N/A'
 
-        final_postings.append((post_title, post_url, post_price, post_img))
+            post_img = post.find('img').get('src')
+
+            final_postings.append((post_title, post_url, post_price, post_img))
+
+    else:
+        context = {
+            'search': search,
+        }
+        return render(request, 'my_app/404error.html', context)
 
     context = {
         'search': search,
